cb315bcc826b71f86337056c0057da1375b3d1f2
Severen Redwood <severen@shrike.me>
2020-12-06 09:23:57 +1300

    Implement game logic for the bell game

---
+++ Assets/Scripts/BellRinger.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+
+[RequireComponent(typeof(AudioSource))]
+public class BellRinger : MonoBehaviour {
+  public void Ring() {
+    var audioSource = GetComponent<AudioSource>();
+    audioSource.Play();
+  }
+
+  void OnMouseDown() {
+    var sequencer = GameObject.Find("Sequencer").GetComponent<BellSequencer>();
+
+    if (gameObject == sequencer.Last) {
+      sequencer.Success();
+    } else {
+      sequencer.Fail();
+    }
+  }
+}
--- Assets/Scripts/BellSequencer.cs
+++ Assets/Scripts/BellSequencer.cs
@@ -4,30 +4,63 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class BellSequencer : MonoBehaviour {
-  public HandbellRinger[] handbells;
-  public float timeInterval = 0.75f;
+  /// <summary>
+  /// The available bells to ring.
+  /// </summary>
+  public BellRinger[] Bells;
+  /// <summary>
+  /// The time interval between bell rings.
+  /// </summary>
+  public float TimeInterval = 0.75f;
+
+  /// <summary>
+  /// The current sequence of bells.
+  /// </summary>
+  public List<GameObject> CurrentSequence;
+  /// <summary>
+  /// The last bell in the sequence.
+  /// </summary>
+  public GameObject Last =>
+    CurrentSequence[CurrentSequence.Count - 1].gameObject;
 
   void Start() {
-    var sequence = GenerateSequence(5);
-
-    StartCoroutine(RingHandbells(sequence));
+    NewSequence();
+    StartCoroutine(RingBells(CurrentSequence));
   }
 
-  List<HandbellRinger> GenerateSequence(int length) {
-    var sequence = new List<HandbellRinger>();
+  public void Success() {
+    Last.GetComponent<BellRinger>().Ring();
+
+    CurrentSequence.RemoveAt(CurrentSequence.Count - 1);
+  }
+
+  public void Fail() {
+    // Play the failure sound.
+    GetComponent<AudioSource>().Play();
+
+    NewSequence();
+    StartCoroutine(RingBells(CurrentSequence, 1.5f));
+  }
+
+  public void NewSequence() => CurrentSequence = GenerateSequence(5);
+
+  List<GameObject> GenerateSequence(int length) {
+    var sequence = new List<GameObject>();
 
     for (int i = 0; i < length; i++) {
-      int j = Random.Range(0, handbells.Length);
-      sequence.Add(handbells[j]);
+      int j = Random.Range(0, Bells.Length);
+      sequence.Add(Bells[j].gameObject);
     }
 
     return sequence;
   }
 
-  IEnumerator RingHandbells(IEnumerable<HandbellRinger> handbells) {
-    foreach (var handbell in handbells) {
-      handbell.Ring();
-      yield return new WaitForSeconds(timeInterval);
+  IEnumerator RingBells(IEnumerable<GameObject> bells, float delay = 0) {
+    yield return new WaitForSeconds(delay);
+
+    foreach (var bell in bells) {
+      bell.GetComponent<BellRinger>().Ring();
+      yield return new WaitForSeconds(TimeInterval);
     }
   }
 }
