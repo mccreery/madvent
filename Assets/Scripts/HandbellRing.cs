6f43916fdbac7f376d421941f75d6e97fb4f2465
Severen Redwood <severen@shrike.me>
2020-12-05 15:07:09 +1300

    Implement basic bell behaviour

---
+++ Assets/Scripts/HandbellRing.cs
@@ -0,0 +1,9 @@
+﻿using UnityEngine;
+
+[RequireComponent(typeof(AudioSource))]
+public class HandbellRing : MonoBehaviour {
+    public void OnMouseDown() {
+        var audioSource = GetComponent<AudioSource>();
+        audioSource.Play();
+    }
+}
