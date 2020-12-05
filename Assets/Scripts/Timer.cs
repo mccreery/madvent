9022674d326b1a63642b85834d609912e98b27ef
Sam McCreery <4602020+mccreery@users.noreply.github.com>
2020-12-05 12:41:52 +0000

    Add snowball fight timer

---
+++ Assets/Scripts/Timer.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using UnityEngine;
+using UnityEngine.UI;
+
+[RequireComponent(typeof(Text))]
+public class Timer : MonoBehaviour
+{
+    public float startTime = 60;
+
+    private Text text;
+
+    private void Start()
+    {
+        text = GetComponent<Text>();
+    }
+
+    private void Update()
+    {
+        float seconds = startTime - Time.timeSinceLevelLoad;
+        TimeSpan timespan = TimeSpan.FromSeconds(seconds);
+        text.text = timespan.ToString("mm\\:ss");
+    }
+}
