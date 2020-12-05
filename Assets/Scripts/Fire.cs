694ad67663e7b75dcedd82f18f370c6221de8170
Sam McCreery <4602020+mccreery@users.noreply.github.com>
2020-12-05 12:08:22 +0000

    Snowball fight throwing and basic targets

---
+++ Assets/Scripts/Fire.cs
@@ -0,0 +1,55 @@
+﻿using UnityEngine;
+
+public class Fire : MonoBehaviour
+{
+    [SerializeField]
+    private GameObject prefab = null;
+
+    [SerializeField]
+    private float cooldown = 0.1f;
+
+    [SerializeField]
+    private float force = 1000;
+
+    [SerializeField]
+    private float startDistance = -5;
+
+    [SerializeField]
+    private AudioClip fireSound = null;
+
+    private new Camera camera;
+    private AudioSource source;
+    private void Start()
+    {
+        camera = Camera.main;
+        source = GetComponent<AudioSource>();
+    }
+
+    private float timeSinceSpawn;
+    void Update()
+    {
+        if (Input.GetButtonDown("Fire1"))
+        {
+            timeSinceSpawn += Time.deltaTime;
+            if (timeSinceSpawn >= cooldown)
+            {
+                Spawn(prefab, force);
+                source.PlayOneShot(fireSound);
+                timeSinceSpawn = 0;
+            }
+        }
+        else
+        {
+            timeSinceSpawn = cooldown;
+        }
+    }
+
+    private GameObject Spawn(GameObject prefab, float force)
+    {
+        Ray ray = camera.ScreenPointToRay(Input.mousePosition);
+
+        GameObject spawned = Instantiate(prefab, ray.GetPoint(startDistance), Random.rotation);
+        spawned.GetComponent<Rigidbody>().AddForce(ray.direction * force);
+        return spawned;
+    }
+}
---
+++ Assets/Scripts/Target.cs
@@ -0,0 +1,46 @@
+﻿using System.Collections;
+using UnityEngine;
+
+public class Target : MonoBehaviour
+{
+    public string snowballTag = "snowball";
+    public float recoveryTime = 3.0f;
+
+    public GameObject targetModel;
+    public Quaternion hitRotation;
+    public float rotationSpeed = 90f;
+
+    private bool hit = false;
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.gameObject.tag == snowballTag)
+        {
+            hit = true;
+            Destroy(other.gameObject);
+            StartCoroutine(recoverTarget());
+        }
+    }
+
+    private IEnumerator recoverTarget()
+    {
+        yield return new WaitForSeconds(recoveryTime);
+        hit = false;
+    }
+
+    private void Update()
+    {
+        Quaternion modelRotation;
+
+        if (hit)
+        {
+            modelRotation = Quaternion.RotateTowards(targetModel.transform.rotation, hitRotation, rotationSpeed);
+        }
+        else
+        {
+            modelRotation = Quaternion.RotateTowards(hitRotation, targetModel.transform.rotation, rotationSpeed);
+        }
+
+        targetModel.transform.rotation = modelRotation;
+    }
+}
