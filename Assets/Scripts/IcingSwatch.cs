3590f32d5597d12801a526735e9d2062f94996b2
Sam McCreery <4602020+mccreery@users.noreply.github.com>
2020-12-06 12:53:57 +0000

    Make icing tubes move towards player a bit

--- Assets/Scripts/IcingSwatch.cs
+++ Assets/Scripts/IcingSwatch.cs
@@ -6,6 +6,32 @@ public class IcingSwatch : MonoBehaviour
     public GameObject cursor;
     public Color color;
 
+    public Vector3 highlightOffset;
+    private Vector3 initialPosition;
+
+    private void Start()
+    {
+        initialPosition = transform.position;
+    }
+
+    private Vector3 velocity;
+    private void Update()
+    {
+        Vector3 targetPosition = hovered ? initialPosition + highlightOffset : initialPosition;
+        transform.position = Vector3.SmoothDamp(transform.position, targetPosition, ref velocity, 0.1f);
+    }
+
+    private bool hovered = false;
+    private void OnMouseEnter()
+    {
+        hovered = true;
+    }
+
+    private void OnMouseExit()
+    {
+        hovered = false;
+    }
+
     public void OnMouseDown()
     {
         cursor.transform.position = cookieDraw.cursor.position;
