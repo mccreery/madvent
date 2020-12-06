e126421282329a979f8a8fc43a7636d64b05c7b7
Sam McCreery <4602020+mccreery@users.noreply.github.com>
2020-12-06 14:15:17 +0000

    Add design script to change cookie design

---
+++ Assets/Scripts/CookieDesign.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+
+public class CookieDesign : MonoBehaviour
+{
+    public Material baseMaterial;
+    public MeshRenderer renderer;
+
+    public Texture2D initialDesign;
+
+    private void Start()
+    {
+        SetDesign(initialDesign);
+    }
+
+    public void SetDesign(Texture2D design)
+    {
+        Material material = new Material(baseMaterial);
+        material.mainTexture = design;
+        renderer.material = material;
+    }
+}
